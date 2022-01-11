66e22fa21b9c9ba1ae4d24ae8a6914734dc149db
Ryan Ashby <rjapsu160@gmail.com>
2022-01-10 16:02:09 -0800

    write second and third functions for cypher
    Co-authored-by: Alex Adamovic <alexanderadamovic@gmail.com>
    Co-authored by:Petia Kortcheva<peetiak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,15 @@ console.log(firstLetter);
 console.log(lastLetter);
 
 function capLetters(userSentence) {
-  let firstLetter = userSentence.charAt(0).toUpperCase();
-  let lastLetter = userSentence.charAt(userSentence.length-1).toUpperCase(); 
-  return firstLetter.concat(lastLetter);
+  let newString = firstLetter.concat(lastLetter);
+  return newString;
 }
+
+function reverseCapLetters(userSentence) {
+  let newReverseString = firstLetter.concat(lastLetter).split("").reverse().join("");
+  return userSentence.concat(newReverseString);
+}
+
+function third (userSentence) {
+//   return capLetters(userSentence).concat(reverseCapLetters(userSentence));
+// }
